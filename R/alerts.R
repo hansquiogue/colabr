5447008c222cc15027555aba38e275942ade08b6
Hans Quiogue <hansquiogue@gmail.com>
2021-01-30 02:22:24 -0500

    Updated default width and fixed embedding issue

--- R/alerts.R
+++ R/alerts.R
@@ -2,21 +2,22 @@
 #'
 #' Display a link or an embedded webpage that helps generate Colab alerts.
 #'
+#' @importFrom IRdisplay display_html
 #' @param embeded A boolean value on whether or not to display an embedded version
 #' of the alert webpage. If set to false, a link to the webpage is printed instead.
 #' @param width A numeric value of the width to display the embedded webpage.
 #' @param height A numeric value of the height to display the embedded webpage.
 #' @export
-generate_alerts <- function(embedded = TRUE, width = 800, height = 500) {
+generate_alerts <- function(embedded = TRUE, width = 1000, height = 500) {
 
   # String of HTML code to display embedded webpage
-  html_str <- paste('<iframe src="quiogue.me/google-colab-alerts" width=',
+  html_str <- paste('<iframe src="https://quiogue.me/google-colab-alerts" width=',
                     as.character(width), " height=", as.character(height),
                     '></iframe>', sep = "")
 
   # Display embedded webpage
   if (embedded) {
-    IRdisplay::display_html(as.character(cat(html_str)))
+    display_html(html_str)
 
   # Print webpage link
   } else {
