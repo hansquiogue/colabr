f124d0e31a1480eb8e887426c0cc3a6712b9b37a
Hans Quiogue <hansquiogue@gmail.com>
2021-02-01 15:47:59 -0500

    Added embedding and ports as arguments

--- R/text2vec.R
+++ R/text2vec.R
@@ -1,11 +1,20 @@
 #' Plot LDA models from text2vec in Google Colab
 #'
+#' Allows LDA models from the text2vec package to be plotted
+#' in Google Colab. A domain is generated to view either as
+#' embedded or as a standalone link.
+#'
 #' @param lda_model A lda model object from text2vec.
+#' @param embedded A boolean value on whether or not an embedded
+#' HTML page is displayed. Otherwise, a link is generated.
+#' @param port A port number to host tunneled server.
+#' Default value is 4321.
 #' @export
-lda_plot_text2vec <- function(lda_model) {
+plot_lda_text2vec <- function(lda_model, embedded = TRUE, port = 4321) {
 
   # TODO: Error checking
   # TODO: Port as argument
+  # TODO: Seperate installation for npm installation
 
   # Download localtunnel
   system('npm install -g localtunnel')
@@ -19,10 +28,20 @@ lda_plot_text2vec <- function(lda_model) {
   system('killall lt')
 
   # Run localtunnel
-  system(paste('lt --port 4321 --subdomain ', domain, ' &', sep = ''))
+  system(paste('lt --port ', as.character(port),' --subdomain ', domain, ' &', sep = ''))
 
-  # Message that domain is generated
-  print(paste('View plot at domain: http://', domain, '.loca.lt', sep = ''))
+  # Embedding is shown
+  if (embedded) {
+    IRdisplay::display_html(paste('<iframe src="http://', domain, '.loca.lt"',
+                                  ' width=800 height=800></iframe>', sep = ''))
+
+  # Embedding is not shown
+  } else {
+    # Message that domain is generated
+    print(paste('View plot at domain: http://', domain, '.loca.lt', sep = ''))
+  }
+
+  # Other informative messages
   print('Do not stop running this code until you are done with the visuals.')
   print('Ignore local domain that is generated below.')
 
